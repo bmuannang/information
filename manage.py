dcdf326789cfcc3f23e4ee8aede4b999c7ce5928
bmuannang <jrdc97khqw0161@126.com>
2021-06-02 18:43:43 +0800

    session 的保存位置指定 & 测试

--- manage.py
+++ manage.py
@@ -1,4 +1,7 @@
 from flask import Flask
+#此模块用来指定session的保存位置
+from flask import session
+from flask.ext.session import Session
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import CSRFProtect
 from redis import StrictRedis
@@ -8,6 +11,7 @@ class Config(object):
     '''项目配置'''
     DEBUG = True
 
+    SECRET_KEY = 'VKBK0fOa/ogY93KyFcM4U1yCB+hDdrSZrcztRBJGqOKrDz1Gtcrce9XYtJNcRIMr'
     #为数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -15,6 +19,20 @@ class Config(object):
     #redis的配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+
+    #session保存配置
+    SESSION_TYPE = 'redis'
+    #开启session签名
+    SESSION_USE_SIGNER = True
+    #制定session保存的redis
+    SESSION_REDIS = StrictRedis(host = REDIS_HOST,port=REDIS_PORT)
+    #设置过期
+    SESSON_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
+
+
 
 app = Flask(__name__)
 
@@ -27,14 +45,15 @@ db = SQLAlchemy(app)
 #初始化redis存储对象
 redis_store = StrictRedis(host = Config.REDIS_HOST,port = Config.REDIS_PORT)
 
-
-
+#开启当前项目 CSRF 保护，只做服务器验证功能
 CSRFProtect(app)
 
-
+#设置session保存制定位置
+Session(app)
 
 @app.route('/')
 def index():
+    session["name"] = "itheima"
     return 'index'
 
 
